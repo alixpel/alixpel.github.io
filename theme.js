f5a28bf00faa7a3b89f4aba58a00f2612fb82213
Alix Pelletier <alixpelletierpro@gmail.com>
2020-04-12 20:54:52 +0200

    fix js

--- theme.js
+++ theme.js
@@ -1,21 +1,8 @@
-/*
-Pour cet exercice, le but est de coder la fonctionalité suivante :
-
-- Il doit y avoir deux boutons en haut de la page :
-  un bouton « Thème clair » et un bouton « Thème sombre ».
-- Quand on clique sur le bouton « Thème clair », les textes de la page
-  doivent s’afficher en noir sur fond blanc.
-- Quand on clique sur le bouton « Thème sombre », les textes de la page
-  doivent s’afficher en blanc sur fond noir.
-
-Vous pouvez utiliser toutes les manières que vous voulez pour ça,
-tant que ça fonctionne :)
-*/
 // nuit
 document.querySelector('.night-button').addEventListener('click', function(){
-	document.querySelector("h1").style.color = 'white';
-	document.querySelector("p").style.color = 'white';
-	document.body.style.backgroundColor = 'black';
+	document.querySelector("h1").style.color = '#F4FFFD';
+	document.querySelector("p").style.color = '#F4FFFD';
+	document.body.style.backgroundColor = '#011936';
     document.querySelector('.jacquouillejour').style.display = "none";
     document.querySelector('.jacquouillenuit').style.display = "block";
 });
@@ -23,10 +10,13 @@ document.querySelector('.night-button').addEventListener('click', function(){
 // jour
 
 document.querySelector('.day-button').addEventListener('click', function(){
-	document.querySelector("h1").style.color = 'black';
-	document.querySelector("p").style.color = 'black';
-	document.body.style.backgroundColor = 'white';
+	document.querySelector("h1").style.color = '#011936';
+	document.querySelector("p").style.color = '#011936';
+	document.body.style.backgroundColor = '#F4FFFD';
     document.querySelector('.jacquouillenuit').style.display = "none";
     document.querySelector('.jacquouillejour').style.display = "block";
 });
     document.querySelector('.jacquouillenuit').style.display = "none";
+    document.body.style.backgroundColor = '#F4FFFD';
+    document.querySelector("h1").style.color = '#011936';
+  document.querySelector("p").style.color = '#011936';
