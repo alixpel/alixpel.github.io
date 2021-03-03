7cdc9c5eea4e6917fd55e9de9943007099f58b94
Alix Pelletier <alixpelletierpro@gmail.com>
2021-03-04 00:45:05 +0100

    correct indentation

--- theme.js
+++ theme.js
@@ -3,20 +3,19 @@ document.querySelector('.night-button').addEventListener('click', function(){
 	document.querySelector("h1").style.color = '#F4FFFD';
 	document.querySelector("p").style.color = '#F4FFFD';
 	document.body.style.backgroundColor = '#011936';
-    document.querySelector('.jacquouillejour').style.display = "none";
-    document.querySelector('.jacquouillenuit').style.display = "block";
+	document.querySelector('.jacquouillejour').style.display = "none";
+	document.querySelector('.jacquouillenuit').style.display = "block";
 });
 
 // jour
-
 document.querySelector('.day-button').addEventListener('click', function(){
 	document.querySelector("h1").style.color = '#011936';
 	document.querySelector("p").style.color = '#011936';
 	document.body.style.backgroundColor = '#F4FFFD';
-    document.querySelector('.jacquouillenuit').style.display = "none";
-    document.querySelector('.jacquouillejour').style.display = "block";
+    	document.querySelector('.jacquouillenuit').style.display = "none";
+    	document.querySelector('.jacquouillejour').style.display = "block";
 });
-    document.querySelector('.jacquouillenuit').style.display = "none";
-    document.body.style.backgroundColor = '#F4FFFD';
-    document.querySelector("h1").style.color = '#011936';
-  document.querySelector("p").style.color = '#011936';
+document.querySelector('.jacquouillenuit').style.display = "none";
+document.body.style.backgroundColor = '#F4FFFD';
+document.querySelector("h1").style.color = '#011936';
+document.querySelector("p").style.color = '#011936';
